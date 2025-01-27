758cb69c8eaf8a9825de2e0cd34a3f02214398e4
Yaskabro <jako169@hotmail.it>
2025-01-27 09:24:45 +0100

    synchronized style with register pages

--- app/src/pages/Login.jsx
+++ app/src/pages/Login.jsx
@@ -8,24 +8,24 @@ const Login = () => {
         <>
             <div className='flex w-screen h-screen overflow-hidden'>
                 <div className='flex flex-1 justify-center items-center h-screen bg-slate-50'>
-                    <img src={logo} alt="logo" className="fixed top-2 left-2 w-24 h-auto" />
+                    <img src={logo} alt="logo" className="fixed top-2 left-2 w-24 h-24" />
                     <div className='w-full max-w-md p-6'>
                         <div className='form-login'>
                             <h2 className='text-secondary'>Bentornato!</h2><br />
                             <form action="">
-                                <label htmlFor="email" className=" text-secondary">Indirizzo Email:</label><br />
-                                <input className="w-full border-2 border-gray-300 focus:border-secondary rounded-lg py-1" type="email" id="email" name="email" required /> <br /><br />
-                                <label htmlFor="password" className=" text-secondary">Password:</label><br />
-                                <input className="w-full border-2 border-gray-300 focus:border-secondary rounded-lg py-1" type="password" id="password" name="password" required /> <br />
+                                <label htmlFor="email" className="block text-sm font-medium text-secondary">Indirizzo Email:</label><br />
+                                <input className="mt-1 shadow-md bg-slate-50 border border-slate-100 text-gray-900 text-sm rounded-lg block w-full p-2.5 hover:border-slate-500 focus:outline-none focus:ring-0" type="email" id="email" name="email" required /> <br /><br />
+                                <label htmlFor="password" className="block text-sm font-medium text-secondary">Password:</label><br />
+                                <input className="mt-1 shadow-md bg-slate-50 border border-slate-100 text-gray-900 text-sm rounded-lg block w-full p-2.5 hover:border-slate-500 focus:outline-none focus:ring-0" type="password" id="password" name="password" required /> <br />
                                 <div className="flex justify-between items-center">
                                     <a className="text-secondary underline hover:no-underline" href="Pagina di recupero passwod">Hai dimenticato la tua Password? </a>
-                                    <button type="submit" className="bg-secondary text-white py-2 px-4 rounded-lg my-4 ml-auto">Accedi</button> <br />
+                                    <button type="submit" className="bg-secondary text-slate-50 p-2 py-2.5 rounded-lg text-sm font-medium hover:bg-primary hover:text-secondary my-4 ml-auto">Accedi</button> <br />
                                 </div>
                             </form>
                         </div>
                         <div className="border-t-2 border-gray-300">
-                            <br /><span>Non hai ancora un account? </span>
-                            <Link to="/register" className="text-violet-700" href="Link di registrazione"> Iscriviti Ora</Link>
+                            <br /><span className="font-medium text-secondary">Non hai ancora un account? </span>
+                            <Link to="/register" className="text-violet-700 cursor-pointer" href="Link di registrazione"> Iscriviti Ora</Link>
                         </div>
                     </div>
                 </div>
